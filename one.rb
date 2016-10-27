e0699374daa39a4b0e55e245843231bbc5593c51
Abdullah Velioglu <abdullahvelioglu08@gmail.com>
2016-10-27 23:56:39 +0200

    Homework-Abdullah Velioglu

--- one.rb
+++ one.rb
@@ -1,2 +1,24 @@
-#Birinci Ödev
-#my-github-username
+puts """Lütfen Sırasıyla
+        Yaşınızı
+        Adınızı ve
+        Okulunuzu giriniz.
+        Örnek 20 (Enter) Abdullah (Enter) KTU(Enter)
+Giriş:"""
+age = gets.chomp.to_i
+name = gets.chomp.to_s
+scholl = gets.chomp.to_s
+me = [age,name,scholl]
+sum = me[1].length + me[2].length
+if sum > 7
+  me.push("7'den Buyuk")
+else
+  me.clear
+  me.push("7'den küçük")
+end
+i=0
+puts "\nÇıkış:\n"
+while i <= me.length-1 do
+  puts me[i]
+  i+=1
+end
+
