0b9300e813d58ed013bb1cd3c86e59cad6d468bb
Abdullah Velioğlu <abdullahvelioglu.295086@hotmail.com>
2016-10-28 00:20:15 +0200

    BUG

--- one.py
+++ one.py
@@ -3,7 +3,7 @@ name = input("Adiniz? ")
 school = input("Okulunuz? ")
 
 me = [age,name,school]
-sum = int(me[0])+ len(me[1]) + len(me[2])
+sum = int(me[0])+ len(me[1]) + len(me[2])  #İşlem hatası var. Dökümantasyonda sadece name ve school'un toplamını istiyor.
 
 if sum > 7:
 	me.append("7'den büyük")
