95793699183978cd6aff5118a0df7f5af659913f
ecenurozturk <ecenurrozturk@gmail.com>
2016-10-27 23:30:43 +0300

    birinci ödev

--- one.rb
+++ one.rb
@@ -1,2 +1,23 @@
-#Birinci Ödev
-#my-github-username
+@me=[]
+puts"How old are you?"
+age=gets.chomp
+@me.push(age)
+
+puts "What is your name?"
+name=gets.chomp
+@me.push(name)
+
+puts "What is your school name?"
+school=gets.chomp
+@me.push(school)
+
+@sum = name.length + school.length
+
+if @sum >7
+	@me.push("bigger than seven")
+else
+	@me.clear
+	@me.push("smaller than seven")
+end
+
+puts @me
