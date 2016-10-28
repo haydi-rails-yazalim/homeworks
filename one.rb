a8def02ad0fc13a505cb42738122312d4dd5caed
bugce12 <bugce2497@gmail.com>
2016-10-28 17:51:47 +0300

    birinci ödev

--- one.rb
+++ one.rb
@@ -1,2 +1,25 @@
 #Birinci Ödev
-#my-github-username
+#bugcekorkmaz
+
+puts "Yas giriniz: "
+age = gets.chomp
+puts "Ad giriniz: "
+name = gets.chomp
+puts "Okul giriniz: "
+school = gets.chomp
+
+@me = [age,name,school]
+@sum = @me[1].length + @me[2].length
+
+if @sum>7
+@me.push("7'den büyük")
+
+else
+@me.pop
+@me.pop
+@me.pop
+@me.push("7'den küçük")
+end
+
+puts ""
+puts @me
