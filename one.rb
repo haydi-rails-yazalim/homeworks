1e9c14904e2f7b4d10ee062398d76fc5dba8da0f
maxmin099 <fatihaydin138@gmail.com>
2016-10-28 20:24:24 +0300

    birinci ödev

--- one.rb
+++ one.rb
@@ -1,2 +1,21 @@
 #Birinci Ödev
-#my-github-username
+#maxmin099
+puts "Age giriniz..."
+@age = gets.chomp
+puts "Name giriniz..."
+@name = gets.chomp
+puts "School giriniz..."
+@school = gets.chomp
+@me=[@age,@name,@school]
+@sum = @me[1].length + @me[2].length
+if @sum>7
+ @me.push("7'den büyük")
+else
+ @me=Array.new(0)
+ @me[0]=("7'den küçük")
+end
+puts
+puts "Name ve School'un harf sayısı toplamı:#{@sum}"
+puts "Dizinin Çıktısı:"
+puts
+puts @me
